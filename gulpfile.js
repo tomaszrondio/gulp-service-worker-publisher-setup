827fc5ba4c79e1290a920a9d452d0e606e11173b
Tomasz Rondio <tomasz.rondio@gmail.com>
2020-04-01 13:16:41 +0200

    update to gulp 4

--- gulpfile.js
+++ gulpfile.js
@@ -1,136 +1,94 @@
-'use strict';
+const { watch, src, dest, series, parallel } = require('gulp');
+const babel = require('gulp-babel');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
+const rename = require('gulp-rename');
+const del = require('del');
+const postcss = require('gulp-postcss');
+const sass = require('gulp-sass');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+const replace = require('gulp-replace')
 
-var gulp = require('gulp')
-var inject = require('gulp-inject')
-var sass = require('gulp-sass')
-var importCss = require('gulp-import-css')
-var autoprefixer = require('gulp-autoprefixer')
-var sourcemaps = require('gulp-sourcemaps')
-var concat = require('gulp-concat')
-var cssmin = require('gulp-cssmin')
-var watch = require('gulp-watch')
-var swPrecache = require('sw-precache')
-var uglify = require('gulp-uglify')
-var replace = require('gulp-replace')
+const config = {
+  app: {
+    js: [
+      './public/js/vendor/*js',
+      './public/js/scripts/*.js'
+    ],
+    sass: './public/css/**/*.sass',
+    htmlFile: './views/base.html.twig'
+  },
+  dist: {
+    base: './public/dist/',
+    html: './views/'
+  }
+}
 
+let hashCode = new Date().getTime();
 
+function jsDev(done) {
+  src(config.app.js, { sourcemaps: true })
+    .pipe(babel({
+      presets: ['@babel/preset-env']
+    }))
+    .pipe(concat('bundle.dev.js'))
+    .pipe(dest(config.dist.base, { sourcemaps: true }))
+  done();
+}
 
-gulp.task('version', function () {
-    var time = new Date().getTime()
-    return gulp.src(['views/base.html.twig'])
-    .pipe(replace(/version\s=\s\'\d*\'/g, 'version = \''+time+'\''))
-    .pipe(gulp.dest('views'));
+function jsBuild(done) {
+  src(config.app.js)
+    .pipe(babel({
+      presets: ['@babel/preset-env']
+    }))
+    .pipe(concat('bundle.js'))
+    .pipe(uglify())
+    .pipe(rename({ suffix: '.' + hashCode }))
+    .pipe(dest(config.dist.base))
+  done();
+}
 
-});
+function cssBuild(done) {
+  src(config.app.sass)
+    .pipe(concat('bundle.sass'))
+    .pipe(sass({ outputStyle: 'expanded' }))
+    .pipe(postcss([autoprefixer(), cssnano()]))
+    .pipe(rename({ suffix: '.' + hashCode }))
+    .pipe(dest(config.dist.base))
+  done();
+}
 
+function cssDev(done) {
+  src(config.app.sass, { sourcemaps: true })
+    .pipe(concat('bundle.dev.sass'))
+    .pipe(sass({ outputStyle: 'expanded' }))
+    .pipe(dest(config.dist.base, { sourcemaps: true }))
+  done();
+}
 
+function setHash(done) {
+  src(config.app.htmlFile)
+    .pipe(replace(/assetsHash\s=\s\'.*\'/g, 'assetsHash = \'' + hashCode + '\''))
+    .pipe(dest(config.dist.html));
+  done();
+};
 
-// service worker generator
-gulp.task('sw', function(callback) {
-  var packageJson = require('./package.json');
-  var bundleUrl = '/public/';
+function setDevHash(done) {
+  src(config.app.htmlFile)
+    .pipe(replace(/assetsHash\s=\s\'.*\'/g, 'assetsHash = \'dev\''))
+    .pipe(dest(config.dist.html));
+  done();
+};
 
-  swPrecache.write(
-    'public/sw.js',
-    {
-      cacheId: packageJson.name,
-      // uncomment if you want to control content caching by yourself or have custom offline page
-      // importScripts: ['/public/sw-contentCaching.js'],
+function watchFiles() {
+  watch(config.app.js, jsDev);
+  watch(config.app.sass, cssDev);
+}
 
-      //runtime caching.
-      runtimeCaching: [{
-        // matches googleapis cdn's. Fonts, jquery etc.
-        urlPattern: /(googleapis|gstatic)/,
-        handler: 'cacheFirst',
-        options: {
-          cache: {
-            maxEntries: 200,
-            name: 'googleapis-cache'
-          }
-        }
-      },
-      {
-        // matches media content from publisher. Images etc.
-        urlPattern: /(media)/,
-        handler: 'cacheFirst',
-        options: {
-          cache: {
-            maxEntries: 50,
-            name: 'media-cache'
-          }
-        }
-      }
-      ],
-      //assets precache
-      staticFileGlobs: ['public/dist/**/*.{js,html,css,png,jpg,gif,svg,eot,ttf,woff}','public/img/**/*.{js,html,css,png,jpg,gif,svg,eot,ttf,woff}', 'public/fonts/**/*.{svg,eot,ttf,woff}'],
-      stripPrefixMulti: {
-        'public/': bundleUrl
-      }
+function cleanUp() {
+  return del([config.dist.base]);
+}
 
-    },
-    callback
-
-    );
-});
-
-
-gulp.task('sass', function(){
- return gulp.src('public/dist/style.scss')
- .pipe(inject(gulp.src(['public/css/**/*.css', 'public/css/**/*.+(sass|scss)'], {read: false}), {
-     starttag: '/*injector*/',
-     endtag: ' /*endinjector*/',
-     transform: function (filePath, file) {
-       filePath = filePath.replace('/public/css/','../css/');
-       return '\n@import \'' + filePath + '\';';
-     }
-   })
- )
-  .pipe(sourcemaps.init())
-  .pipe(sass.sync().on('error', sass.logError))
-  .pipe(sourcemaps.write('./'))
-  .pipe(gulp.dest("public/dist/"));
-
-
-});
-
-
-
-gulp.task('js',function(){
- return gulp.src(['public/js/vendor/**/*.js','public/js/scripts/**/*.js',,'public/js/*.js'])
-     .pipe(sourcemaps.init())
-     .pipe(concat('all.js'))
-     .pipe(sourcemaps.write())
-     .pipe(gulp.dest('public/dist'));
-
-
-});
-
-gulp.task('cssmin', ['sass'],function(){
-  return gulp.src('public/dist/style.css')
-  .pipe(importCss())
-  .pipe(autoprefixer({ browsers: ['last 2 version'], cascade: false }))
-  .pipe(cssmin())
-  .pipe(gulp.dest('public/dist'));
-
-});
-
-gulp.task('jsmin', ['js'],function(){
-  return gulp.src('public/dist/all.js')
-  .pipe(uglify())
-  .pipe(gulp.dest('public/dist'));
-
-});
-
-
-
-gulp.task('build', ['sass', 'js', 'cssmin' , 'jsmin','version', 'sw' ]);
-
-gulp.task('watch', function() {
-  //empty service worker
-  require('fs').writeFileSync('public/sw.js', '');
-  //
-  gulp.watch('public/css/**/*.+(css|sass|scss)', ['sass']);
-  gulp.watch('public/js/**/*.js', ['js']);
-
-
-});
+exports.watch = series(cleanUp, setDevHash, parallel(jsDev, cssDev, watchFiles));
+exports.build = series(cleanUp, parallel(jsBuild, cssBuild, setHash));
